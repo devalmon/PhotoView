807bc797aac4489348ccf6614af9610ffe0dd940
devalmon <phlood@yandex.ru>
2019-09-14 08:49:12 +0300

    Initial Commit

---
+++ PhotoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoView
+//
+//  Created by Alexey Baryshnikov on 14/09/2019.
+//  Copyright © 2019 Alexey Baryshnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
